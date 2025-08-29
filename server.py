120c2c43724fb33ab67407b4135bf00802460af3
hy992730-hub <hy992730@gmail.com>
2025-08-29 17:25:38 +0530

    Update server.py

--- server.py
+++ server.py
@@ -28,4 +28,4 @@ def generate():
     return base64.b64encode(response.content).decode("utf-8")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8080)
